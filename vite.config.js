12b278af500c64103fbd303f3f9805d73d2b0520
Ashutosh Aman <69684043+AmanAshutosh@users.noreply.github.com>
2026-04-11 21:51:27 +0530

    fix: added vercel.json rewrites and updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // This ensures assets are loaded from the root
+  build: {
+    outDir: "dist",
+  },
+});
